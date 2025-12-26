43091d16c7466d38607e6b68460543305ba2bf6f
Shrinath Rajput <rajputshrinath349@gmail.com>
2025-12-26 13:00:31 +0530

    mangodb database is connected and push the data in mangodb

---
+++ test_mangodb.py
@@ -0,0 +1,15 @@
+#connect the mangodb
+
+from pymongo.mongo_client import MongoClient
+
+uri = "mongodb+srv://rajputshrinath349_db_user:<@password>cluster0.g4gxwbw.mongodb.net/?appName=Cluster0"
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
