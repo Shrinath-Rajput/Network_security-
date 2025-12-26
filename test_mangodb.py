98a9e7a780ba9e3ec9f5cc4cb1f22579c1a3ce00
Shrinath Rajput <rajputshrinath349@gmail.com>
2025-12-26 23:25:43 +0530

    succesfully project

--- test_mangodb.py
+++ test_mangodb.py
@@ -1,15 +1,12 @@
-#connect the mangodb
+from pymongo import MongoClient
 
-from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://rajputshrinath349_db_user:shrinathrajput123@cluster0.g4gxwbw.mongodb.net/?retryWrites=true&w=majority"
 
-uri = "mongodb+srv://rajputshrinath349_db_user:<@password>cluster0.g4gxwbw.mongodb.net/?appName=Cluster0"
-
-# Create a new client and connect to the server
 client = MongoClient(uri)
 
-# Send a ping to confirm a successful connection
 try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    client.admin.command("ping")
+    print("✅ MongoDB connected successfully!")
 except Exception as e:
+    print("❌ MongoDB connection failed")
     print(e)
